e729d6a309a7b3a37d6db6b257f335ea8599d103
fbubeck <ferdinand.bubeck@gmx.de>
2021-01-19 14:11:22 +0100

    Exploration

--- 01 - Data Exploration.R
+++ 01 - Data Exploration.R
@@ -1,6 +1,7 @@
 #import libraries
 library(readr)
 library(dplyr)
+library(ggplot2)
 
 #import CSVs
 datTest <- read_csv("aug_test.csv")
@@ -21,4 +22,47 @@ dim(datFull)
 #count NAs in column gender
 sum(is.na(datFull$gender))
 
+#Plot zu Geschlechterverteilung
+ggplot(data=datFull, aes(x=gender))+
+  geom_bar(color='blue', fill='blue', alpha=.5)+
+  theme_classic()+
+  labs(title="Geschlechterverteilung der Teilnehmer")+
+  xlab("Geschlecht")+
+  ylab("Anzahl")
 
+#Plot der Abschlüsse
+ggplot(data=datFull, aes(x=education_level))+
+  geom_bar(color='orange', fill='orange', alpha=.5)+
+  coord_flip()+
+  theme_classic()+
+  labs(title="Verteilung der Abschlüsse bei den Teilnehmer")+
+  xlab("Art des Abschlusses")+
+  ylab("Anzahl")
+
+#Plot Anzahl der Trainngsstunden und Abschluss (Boxplot)
+ggplot(data=datFull, aes(x=education_level, y=training_hours))+
+  geom_boxplot()+
+  theme_classic()+
+  labs(title="Abschluss und Anzahl der Trainingsstunden")+
+  xlab("Art des Abschlusses")+
+  ylab("Anzahl Trainingsstunden")
+
+#Plot Anzahl der Trainngsstunden und Abschluss (Point/Jitter)
+ggplot(data=datFull, aes(x=education_level, y=training_hours))+
+  geom_jitter(color='black', alpha=.3, size=.9)+
+  theme_classic()+
+  labs(title="Abschluss und Anzahl der Trainingsstunden")+
+  xlab("Art des Abschlusses")+
+  ylab("Anzahl Trainingsstunden")
+
+
+datFull$experience <- sub("<1", "0", datFull$experience)
+datFull$experience <- sub(">20", "21", datFull$experience)
+datFull$experience <- as.numeric(datFull$experience)
+
+ggplot(data=datFull, aes(x=enrolled_university, y=experience))+
+  geom_boxplot(fill="grey", alpha=.7)+
+  theme_classic()+
+  labs(title="Berufserfahrung und Immatrikulatonsstatus")+
+  xlab("Immatrikulatonsstatus")+
+  ylab("Berufserfahrung")
