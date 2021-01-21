ea0131b9c920df1916bf832db475c2ed307e0f8e
fbubeck <ferdinand.bubeck@gmx.de>
2021-01-21 15:34:23 +0100

    added rf

--- Machine Learning Algorithms.R
+++ Machine Learning Algorithms.R
@@ -22,26 +22,49 @@ apply(datTrain.ready, 2, detectNA)
 
 #Model Default
 set.seed(35)
-model.rf <- randomForest(target ~ ., datTrain.ready, ntree=200, mtry = 3, importance=TRUE)
+model.rf.default <- randomForest(target ~ ., datTrain.ready, ntree=200, mtry = 6, importance=TRUE)
 
-model.rf
+model.rf.default
 
-plot(model.rf)
+plot(model.rf.default)
 
 varImpPlot(model.rf,
            sort = T,
            n.var = 5,
            main = "Feature Selection - Top 5 Variables")
 
+
+###
+mtry.searched <- tuneRF(datTrain.ready[,-11], datTrain.ready[,11], mtryStart=5, ntreeTry=100, stepFactor=.8, improve=0.001,
+       trace=TRUE, plot=TRUE, doBest=FALSE)
+
+model.rf.tuned <- randomForest(target ~ ., datTrain.ready, ntree=1000, mtry = 2, do.trace=50, importance=TRUE)
+model.rf.tuned
+
+plot(model.rf.tuned)
+
+varImpPlot(model.rf.tuned,
+           sort = T,
+           n.var = 5,
+           main = "Feature Selection - Top 5 Variables")
+
+
 #Using Cross Validation to tune parameters
+#Create control function for training with 10 folds and keep 3 folds for training. search method is grid.
+control <- trainControl(method='cv', 
+                        number=4, 
+                        search='grid',
+                        verboseIter = TRUE)
+#create tunegrid with 15 values from 1:15 for mtry to tunning model. Our train function will change number of entry variable at each split according to tunegrid. 
+tunegrid <- expand.grid(.mtry = (3:10)) 
 
-#10 fold cross validation
-trControl <- trainControl(method = "cv",
-                          number = 4,
-                          search = "grid")
+rf.grid <- train(target ~ ., 
+                data = datTrain.ready,
+                method = 'rf',
+                metric = 'Accuracy',
+                tuneGrid = tunegrid,
+                trControl = control,
+                importance = TRUE)
 
-model.rf.train <- train(target~.,
-                    data = datTrain.ready,
-                    method = "rf",
-                    metric = "Accuracy",
-                    trControl = trControl)
+print(rf.grid)
+plot(rf.grid)
