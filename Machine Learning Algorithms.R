b1aceb3a90ff2194a6bcb968c964044353a91d88
fbubeck <ferdinand.bubeck@gmx.de>
2021-01-28 15:25:57 +0100

    28.1.

--- Machine Learning Algorithms.R
+++ Machine Learning Algorithms.R
@@ -26,6 +26,10 @@ datTrain_imputed <- complete(impute)
 apply(datTrain_imputed, 2, detectNA)
 
 #Data Balancing
+datTrain_imputed %>% 
+  group_by(target) %>%
+  summarise(no_rows = length(target))
+
 library(groupdata2)
 
 target.balancing <- datTrain_imputed %>% 
@@ -49,6 +53,31 @@ Test <- datTrain_balanced[-trainingRows,]
 library(rpart)
 library(rpart.plot)
 
+##1st tree
+set.seed(34)
+dt.default <- rpart(target~ ., data=Train, cp=-1)
+
+printcp(dt.default)
+plotcp(dt.default)
+
+#Prediction & Performance
+#Train Data
+train.default.pred <- predict(dt.default, newdata = Train, type = "class")
+train.default.confMatrix <- table(train.default.pred, Train[, 12])
+print(train.default.confMatrix)
+
+train.default.accuracy <- sum(diag(train.default.confMatrix))/sum(train.default.confMatrix)
+print(train.default.accuracy)
+
+#Test Data
+test.default.pred <- predict(dt.default, newdata = Test, type = "class")
+test.default.confMatrix <- table(test.default.pred, Test[, 12])
+print(test.default.confMatrix)
+
+test.default.accuracy <- sum(diag(test.default.confMatrix))/sum(test.default.confMatrix)
+print(test.default.accuracy)
+
+##2nd tree with cp limitation
 set.seed(39)
 dt.fit <- rpart(target~ ., data=Train, cp=0.005)
 
